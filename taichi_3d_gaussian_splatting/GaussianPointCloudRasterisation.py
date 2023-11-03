1ede61d99b01ff2e1d01412ffba65e453bf884a1
kaamiiaar <47272408+kaamiiaar@users.noreply.github.com>
2023-11-04 03:50:30 +1100

    make 2 dims

--- taichi_3d_gaussian_splatting/GaussianPointCloudRasterisation.py
+++ taichi_3d_gaussian_splatting/GaussianPointCloudRasterisation.py
@@ -344,8 +344,7 @@ def gaussian_point_rasterisation(
     rgb_only: ti.template(),  # input
 
     # Kaamiiaar
-    pixel_to_gaussians,  # (H, W, MAX_GAUSSIANS)
-
+    pixel_to_gaussians: ti.types.ndarray(ti.i32, ndim=2),  # (H, W, MAX_GAUSSIANS)
 ):
     ti.loop_config(block_dim=(TILE_WIDTH * TILE_HEIGHT))
     for pixel_offset in ti.ndrange(camera_height * camera_width):  # 1920*1080
